8e9254439aa6cc4b845b9d2245f15f597cbc1fc2
idosofi <ido.sofi@gmail.com>
2026-02-10 14:27:07 +0200

    feat: introduce `window.appUI` object with a `test` method for Natively POC UI.

---
+++ bundle.js
@@ -0,0 +1,7 @@
+window.appUI = {
+  test: () => {
+    return `<div class="p-10 bg-green-500 text-white text-center font-bold text-2xl rounded-lg">
+              Natively POC Active! 🚀
+            </div>`;
+  }
+};
